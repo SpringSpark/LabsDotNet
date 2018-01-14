04d1759f26f646a8fa5938d221edaa2b50bc79ee
SpringSpark <artaszx@mail.ru>
2018-01-14 11:40:03 +0300

    Add files via upload

---
+++ MyList/MyList/Class1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myListLibrary
+{
+    public class myList<T> : IList<T>
+    {
+
+        private int _count = 0;
+        
+        class MyNode<T>
+        {
+            public T Data;
+            public MyNode<T> Next;
+        }
+
+        private MyNode<T> _root = null;
+        
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= _count || index < 0)
+                    throw new ArgumentOutOfRangeException();
+                else
+                {
+                    MyNode<T> cur = _root;
+                    for (int i = 0; i < index; i++)
+                    {
+                        cur = cur.Next;
+                    }
+                    return cur.Data;
+                }
+            }
+
+            set
+            {
+                if (index >= _count || index < 0)
+                    throw new ArgumentOutOfRangeException();
+                else
+                {
+                    MyNode<T> cur = _root;
+                    for (int i = 0; i < index && i < _count; i++)
+                    {
+                        cur = cur.Next;
+                    }
+                    cur.Data = value;
+                }
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public void Add(T item)
+        {
+            MyNode<T> newNode = new MyNode<T>();
+            newNode.Data = item;
+            if (_count == 0)
+            {
+                _root = newNode;
+            }
+            else
+            {
+                MyNode<T> cur = _root;
+                for (int i = 0; i < _count - 1; i++)
+                {
+                    cur = cur.Next;
+                }
+                cur.Next = newNode;
+            }
+            _count++;
+        }
+
+        public void Clear()
+        {
+            _count = 0;
+            _root = null;
+        }
+
+        public bool Contains(T item)
+        {
+            bool inList = false;
+            MyNode<T> cur = _root;
+            for (int i = 0; i < _count; i++)
+            {
+                if (cur.Data.Equals(item))
+                {
+                    inList = true;
+                    break;
+                }
+                cur = cur.Next;
+            }
+            return inList;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            int j = arrayIndex;
+            MyNode<T> cur = _root;
+            for (int i = 0; i < _count; i++)
+            {
+                array.SetValue(cur.Data, j);
+                cur = cur.Next;
+                j++;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            MyNode<T> cur = _root;
+            for (int i = 0; i < _count; i++)
+            {
+                yield return cur.Data;
+                cur = cur.Next;
+            }
+        }
+
+        public int IndexOf(T item)
+        {
+            int itemIndex = -1;
+            MyNode<T> cur = _root;
+            for (int i = 0; i < _count; i++)
+            {
+                if (cur.Data.Equals(item))
+                {
+                    itemIndex = i;
+                    break;
+                }
+                cur = cur.Next;
+            }
+            return itemIndex;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index > _count || index < 0)
+                throw new ArgumentOutOfRangeException();
+            else
+            {
+                MyNode<T> newNode = new MyNode<T>();
+                newNode.Data = item;
+                MyNode<T> cur = _root, prev = null;
+                for(int i = 0; i < index; i++)
+                {
+                    prev = cur;
+                    cur = cur.Next;
+                }
+                if(cur == _root)
+                {
+                    newNode.Next = _root;
+                    _root = newNode;
+                }
+                else
+                {
+                    prev.Next = newNode;
+                    newNode.Next = cur;
+                }
+                _count++;
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            bool removed = false;
+            MyNode<T> cur = _root, prev = null;          
+            for (int i = 0; i < _count; i++)
+            {
+                if (cur.Data.Equals(item))
+                {
+                    if (cur == _root)                    
+                        _root = _root.Next;                    
+                    else                    
+                        prev.Next = cur.Next;                    
+                    _count--;
+                    removed = true;
+                    break;
+                }
+                prev = cur;
+                cur = cur.Next;
+            }
+            return removed;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index >= _count || index < 0)
+                throw new ArgumentOutOfRangeException();
+            else
+            {
+                MyNode<T> cur = _root, prev = null;
+                if (index == 0)
+                    _root = _root.Next;
+                else
+                {
+                    for (int i = 0; i < index; i++)
+                    {
+                        prev = cur;
+                        cur = cur.Next;
+                    }
+                    prev.Next = cur.Next;
+                }
+                _count--;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ MyList/UnitTestProjectMyList/UnitTest1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using myListLibrary;
+using System.Collections.Generic;
+
+namespace UnitTestProjectMyList
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public string MyListToString(myList<int> ml)
+        {
+            string s = "";
+            foreach (int x in ml)
+                s += x + " ";
+            return s;
+        }
+
+        [TestMethod]
+        public void Add_ElementsInList()
+        {
+            myList<int> ml = new myList<int>();
+            for (int i = 0; i < 3; i++)
+                ml.Add(i);
+            string expected = "0 1 2 ";
+            string actual = MyListToString(ml);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Replace_ElementsInList()
+        {
+            myList<int> ml = new myList<int>();
+            for (int i = 0; i < 3; i++)
+                ml.Add(i);        
+            for (int i = 0; i < 3; i++)
+                ml[i] = 2 - i;
+            string expected = "2 1 0 ";
+            string actual = MyListToString(ml);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Insert_ElementsInList()
+        {
+            myList<int> ml = new myList<int>();
+            for (int i = 0; i < 3; i++)
+                ml.Insert(i, i);
+            string expected = "0 1 2 ";
+            string actual = MyListToString(ml);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RemoveAt_ElementsFromList()
+        {
+            myList<int> ml = new myList<int>();
+            for (int i = 0; i < 5; i++)
+                ml.Add(i);
+            //0 1 2 3 4
+            for (int i = 4; i >= 0; i -= 2)
+                ml.RemoveAt(i);
+            string expected = "1 3 ";
+            string actual = MyListToString(ml);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Remove_ElementsFromList()
+        {
+            myList<int> ml = new myList<int>();
+            for (int i = 0; i < 5; i++)
+                ml.Add(4 - i);
+            //4 3 2 1 0
+            ml.Remove(3);
+            ml.Remove(0);
+            string expected = "4 2 1 ";
+            string actual = MyListToString(ml);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Contains_ElementsInList()
+        {
+            myList<int> ml = new myList<int>();
+            for (int i = 0; i < 3; i++)
+                ml.Add(i);
+            bool result_1 = ml.Contains(1);
+            bool result_2 = ml.Contains(3);
+            string expected = "True False";
+            string actual = result_1 + " " + result_2;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IndexOf_ElementsInList()
+        {
+            myList<int> ml = new myList<int>();
+            for (int i = 0; i < 3; i++)
+                ml.Add(2 - i);
+            //2 1 0
+            int result_1 = ml.IndexOf(1);
+            int result_2 = ml.IndexOf(2);
+            string expected = "1 0";
+            string actual = result_1 + " " + result_2;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CopyTo_ElementsInList()
+        {
+            myList<int> ml = new myList<int>();
+            for (int i = 0; i < 3; i++)
+                ml.Add(i);
+            int[] array = new int[5];
+            array[0] = 100;
+            array[1] = 99;
+            ml.CopyTo(array, 2);
+            string expected = "100 99 0 1 2 ";
+            string actual = "";
+            foreach (int x in array)
+                actual += x + " ";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Clear_ElementsInList()
+        {
+            myList<int> ml = new myList<int>();
+            for (int i = 0; i < 3; i++)
+                ml.Add(i);
+            ml.Clear();
+            string expected = "";
+            string actual = MyListToString(ml);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
